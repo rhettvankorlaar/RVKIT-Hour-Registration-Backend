b6248d869f48642774f6bf704c4bf57416cc9b06
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-10-02 16:30:24 +0200

    Initial commit

---
+++ src/main/java/nl/rvkit/hourregistration/HourregistrationApplication.java
@@ -0,0 +1,13 @@
+package nl.rvkit.hourregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourregistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HourregistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/rvkit/hourregistration/HourregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.rvkit.hourregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourregistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
