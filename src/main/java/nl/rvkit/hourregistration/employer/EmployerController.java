d75314fa29be03dce752a6f510cd8ce0acd0a5ae
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-10-12 16:15:39 +0200

    Adds Employer service and controller start

--- src/main/java/nl/rvkit/hourregistration/employer/EmployerController.java
+++ src/main/java/nl/rvkit/hourregistration/employer/EmployerController.java
@@ -1,5 +1,13 @@
 package nl.rvkit.hourregistration.employer;
 
+import nl.rvkit.hourregistration.employer.dto.EmployerDTO;
+import nl.rvkit.hourregistration.util.Response;
+import nl.rvkit.hourregistration.util.Routes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +17,13 @@ public class EmployerController {
     public EmployerController(IEmployerService employerService) {
         this.employerService = employerService;
     }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(Routes.Employer.EMPLOYERS)
+    @ResponseStatus(HttpStatus.OK)
+    public Response<EmployerDTO> createEmployer(){
+return null;
+    }
+
+
 }
