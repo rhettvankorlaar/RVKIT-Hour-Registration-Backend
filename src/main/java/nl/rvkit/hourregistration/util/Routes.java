fd7e80d33b01b6dac825026b4643db0c6e2a8b90
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-10-12 16:15:21 +0200

    Adds routes

--- src/main/java/nl/rvkit/hourregistration/util/Routes.java
+++ src/main/java/nl/rvkit/hourregistration/util/Routes.java
@@ -3,7 +3,13 @@ package nl.rvkit.hourregistration.util;
 public class Routes {
     public static final String API_V1 = "/v1";
 
-    public static class User{
-        public static final String USERS = API_V1 + "/users";
+    public static class HourEntry{
+        public static final String HOUR_ENTRIES = API_V1 + "/hourentries";
+    }
+    public static class Project{
+        public static final String PROJECTS = API_V1 + "/projects";
+    }
+    public static class Employer{
+        public static final String EMPLOYERS = API_V1 + "/employers";
     }
 }
